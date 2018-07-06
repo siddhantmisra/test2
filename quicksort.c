7470aa2791c1693d49955d5c9a98885a180c8067
siddhantmisra <metalhead.misra@gmail.com>
2018-07-06 10:06:20 +0530

    updated

--- quicksort.c
+++ quicksort.c
@@ -1,57 +1,57 @@
 #include<stdio.h>
 void quicksort(int number[25],int first,int last)
 {
-   int i, j, pivot, temp;
+int i, j, pivot, temp;
 
-   if(first<last)
-   {
-      pivot=first;
-      i=first;
-      j=last;
+if(first<last)
+{
+pivot=first;
+i=first;
+j=last;
 
-      while(i<j)
-      {
-         while(number[i]<=number[pivot]&&i<last)
-            i++;
-         while(number[j]>number[pivot])
-            j--;
-         if(i<j)
-         {
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
-      }
+while(i<j)
+{
+while(number[i]<=number[pivot]&&i<last)
+  i++;
+while(number[j]>number[pivot])
+  j--;
+if(i<j)
+ {
+ temp=number[i];
+ number[i]=number[j];
+ number[j]=temp;
+ }
+}
 
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
+ temp=number[pivot];
+ number[pivot]=number[j];
+ number[j]=temp;
+ quicksort(number,first,j-1);
+ quicksort(number,j+1,last);
 
-   }
+}
 }
 
 int main(){
-   int i, sum=0, count, number[25];
+int i, sum=0, count, number[25];
 
-   printf("How many elements are u going to enter?: ");
-   scanf("%d",&count);
+printf("How many elements are u going to enter?: ");
+scanf("%d",&count);
 
-   printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
-      sum=sum+number[i];
+printf("Enter %d elements: ", count);
+for(i=0;i<count;i++)
+scanf("%d",&number[i]);
+sum=sum+number[i];
 
-   quicksort(number,0,count-1);
+quicksort(number,0,count-1);
 
-   printf("Order of Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
+printf("Order of Sorted elements: ");
+for(i=0;i<count;i++)
+printf(" %d",number[i]);
    
-   printf("Sum of numbers: ");
-   scanf("%d", &sum);
+printf("Sum of numbers: ");
+scanf("%d", &sum);
 
-   return 0;
+return 0;
 }
 
